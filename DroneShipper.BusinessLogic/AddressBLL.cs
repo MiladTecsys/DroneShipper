08d075f0b06e66739c9deddf6287b443070543eb
OdCyprusHackathon <dirtyWater1>
2015-11-06 10:05:34 -0500

    Distance calc formula

--- DroneShipper.BusinessLogic/AddressBLL.cs
+++ DroneShipper.BusinessLogic/AddressBLL.cs
@@ -3,10 +3,11 @@ using System.Text.RegularExpressions;
 using DroneShipper.BusinessFacade;
 using DroneShipper.DataAccess;
 
-namespace DroneShipper.BusinessLogic
-{
-    public class AddressBLL
-    {
+namespace DroneShipper.BusinessLogic {
+    public class AddressBLL {
+
+        private const double EARTH_RADIUS_KM = 6371;
+
         private readonly AddressDAL _addressDal = null;
 
         public AddressBLL() {
@@ -44,6 +45,25 @@ namespace DroneShipper.BusinessLogic
 
         public void UpdateAddress(AddressInfo address) {
             _addressDal.UpdateAddress(address);
+        }
+
+
+        public static double GetDistanceKm(double fromLat, double fromLong, double toLat, double toLong) {
+            double dLat = ToRad(toLat - fromLat);
+            double dLon = ToRad(toLong - fromLong);
+
+            double a = Math.Pow(Math.Sin(dLat/2), 2) +
+                       Math.Cos(ToRad(fromLat))*Math.Cos(ToRad(toLat))*
+                       Math.Pow(Math.Sin(dLon/2), 2);
+
+            double c = 2*Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            double distance = EARTH_RADIUS_KM*c;
+            return distance;
+        }
+
+        private static double ToRad(double input) {
+            return input*(Math.PI/180);
         }
 
     }
